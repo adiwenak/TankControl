d7d3313816f99886fec846585c9d162b0b1149c6
Addy-Kurnadi <adiwena23@gmail.com>
2013-04-26 17:23:24 +0700

    Commiting EDMX

--- TankControl/TankControl/TankControl.Designer.cs
+++ TankControl/TankControl/TankControl.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Adiwena Kurnadi\Documents\GitHub\TankControl\TankControl\TankControl\TankControlDB.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\Adiwena Kurnadi\Documents\GitHub\TankControl\TankControl\TankControl\TankControl.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
